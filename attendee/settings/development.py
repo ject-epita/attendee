63d81de4586c6bc742eb361e99443d83be1e7841
Quentin Gillet <42375010+Quentin-Gillet@users.noreply.github.com>
2025-10-01 16:39:22 +0200

    Allow all hosts in development settings

--- attendee/settings/development.py
+++ attendee/settings/development.py
@@ -3,7 +3,7 @@ import os
 from .base import *
 
 DEBUG = True
-ALLOWED_HOSTS = ["tendee-stripe-hooks.ngrok.io", "localhost", "attendee.ject.fr"]
+ALLOWED_HOSTS = ["tendee-stripe-hooks.ngrok.io", "localhost", "attendee.ject.fr", "*"]
 
 DATABASES = {
     "default": {
