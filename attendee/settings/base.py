8fb7471ec9245f7ec126e70d15fce5ba1e709362
Quentin Gillet <42375010+Quentin-Gillet@users.noreply.github.com>
2025-10-01 18:04:27 +0200

    Remove S3 storage settings from base.py
    Removed S3 storage configuration from STORAGES.

--- attendee/settings/base.py
+++ attendee/settings/base.py
@@ -197,14 +197,6 @@ SPECTACULAR_SETTINGS = {
 
 STORAGES = {
     "default": {
-        "BACKEND": "storages.backends.s3.S3Storage",
-        "OPTIONS": {
-            "endpoint_url": os.getenv("AWS_ENDPOINT_URL"),
-            "access_key": os.getenv("AWS_ACCESS_KEY_ID"),
-            "secret_key": os.getenv("AWS_SECRET_ACCESS_KEY"),
-        },
-    },
-    "staticfiles": {
         "BACKEND": "django.contrib.staticfiles.storage.StaticFilesStorage",
     },
 }
